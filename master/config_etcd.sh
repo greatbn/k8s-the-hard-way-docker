dd10065f5645aa88fe32248050dd5be2c1b6bdbb
great_bn <great_bn@yahoo.com>
2019-06-28 21:52:08 +0700

    export variable

--- master/config_etcd.sh
+++ master/config_etcd.sh
@@ -1,26 +1,30 @@
-myip="10.20.165.21"
+
+myip="10.20.165.13"
 protocol="https"
 ETCD_DISCOVERY_URL="https://discovery.etcd.io/9d5ff9a89c39c63b40f3d28201520d34"
 cert_dir="/etc/kubernetes/certs"
 
-cat > /etc/etcd/etcd.conf <<EOF
-ETCD_NAME="$myip"
-ETCD_DATA_DIR="/var/lib/etcd/default.etcd"
-ETCD_LISTEN_CLIENT_URLS="$protocol://$myip:2379,http://127.0.0.1:2379"
-ETCD_LISTEN_PEER_URLS="$protocol://$myip:2380"
-ETCD_ADVERTISE_CLIENT_URLS="$protocol://$myip:2379,http://127.0.0.1:2379"
-ETCD_INITIAL_ADVERTISE_PEER_URLS="$protocol://$myip:2380"
-ETCD_DISCOVERY="$ETCD_DISCOVERY_URL"
-ETCD_CA_FILE=$cert_dir/ca.crt
-ETCD_TRUSTED_CA_FILE=$cert_dir/ca.crt
-ETCD_CERT_FILE=$cert_dir/server.crt
-ETCD_KEY_FILE=$cert_dir/server.key
-ETCD_CLIENT_CERT_AUTH=true
-ETCD_PEER_CA_FILE=$cert_dir/ca.crt
-ETCD_PEER_TRUSTED_CA_FILE=$cert_dir/ca.crt
-ETCD_PEER_CERT_FILE=$cert_dir/server.crt
-ETCD_PEER_KEY_FILE=$cert_dir/server.key
-ETCD_PEER_CLIENT_CERT_AUTH=true
+mkdir -p /etc/etcd/
+
+cat > /etc/etcd/etcd.sh <<EOF
+export ETCD_NAME="$myip"
+export ETCD_DATA_DIR="/var/lib/etcd/default.etcd"
+export ETCD_LISTEN_CLIENT_URLS="$protocol://$myip:2379,http://127.0.0.1:2379"
+export ETCD_LISTEN_PEER_URLS="$protocol://$myip:2380"
+export ETCD_ADVERTISE_CLIENT_URLS="$protocol://$myip:2379,http://127.0.0.1:2379"
+export ETCD_INITIAL_ADVERTISE_PEER_URLS="$protocol://$myip:2380"
+export ETCD_DISCOVERY="$ETCD_DISCOVERY_URL"
+export ETCD_CA_FILE=$cert_dir/ca.crt
+export ETCD_TRUSTED_CA_FILE=$cert_dir/ca.crt
+export ETCD_CERT_FILE=$cert_dir/server.crt
+export ETCD_KEY_FILE=$cert_dir/server.key
+export ETCD_CLIENT_CERT_AUTH=true
+export ETCD_PEER_CA_FILE=$cert_dir/ca.crt
+export ETCD_PEER_TRUSTED_CA_FILE=$cert_dir/ca.crt
+export ETCD_PEER_CERT_FILE=$cert_dir/server.crt
+export ETCD_PEER_KEY_FILE=$cert_dir/server.key
+export ETCD_PEER_CLIENT_CERT_AUTH=true
+etcd
 EOF
 
 
